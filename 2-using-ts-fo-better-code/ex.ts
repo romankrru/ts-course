44f54ab954ed059a004fd3774bc615ad51b61fb2
romankrru <rm07ru@gmail.com>
2018-09-30 01:49:50 +0300

    Excercise

---
+++ 2-using-ts-fo-better-code/ex.ts
@@ -0,0 +1,26 @@
+// EXCERCISE
+type BankAccount = {
+	money: number,
+	deposit: (v: number) => void
+}
+
+let bankAccount: BankAccount = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+
+let myself: {
+    name: string,
+    bankAccount: BankAccount,
+    hobbies: string[],
+} = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
