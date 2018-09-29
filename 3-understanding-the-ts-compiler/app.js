72d818964d020aee971fee50f52533b6a563009c
romankrru <rm07ru@gmail.com>
2018-09-30 02:14:57 +0300

    Understanding the compiler

---
+++ 3-understanding-the-ts-compiler/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var myName = "John";
+var myAge = 23;
+var anything;
+anything = 12;
+// myName = 30;
---
+++ 3-understanding-the-ts-compiler/app.ts
@@ -0,0 +1,6 @@
+let myName: string = "John";
+let myAge: number = 23;
+let anything;
+anything = 12;
+
+// myName = 30;
