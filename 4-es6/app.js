326673310a9abdeff1430afd21f22b328a5c72e7
romankrru <rm07ru@gmail.com>
2018-10-01 19:33:56 +0300

    Let and const

---
+++ 4-es6/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// let & const
+var v = "test";
+console.log(v);
+v = "another var";
+console.log(v);
+var maxLevel = 100;
+console.log(maxLevel);
+// maxLevel = 99;
---
+++ 4-es6/app.ts
@@ -0,0 +1,9 @@
+// let & const
+let v = "test";
+console.log(v);
+v = "another var";
+console.log(v);
+
+const maxLevel = 100;
+console.log(maxLevel);
+// maxLevel = 99;
