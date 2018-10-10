3ab52615e6eac5e8cb188747a7ae728698a8d325
romankrru <rm07ru@gmail.com>
2018-10-10 23:38:04 +0300

    Creating a generic function

--- 8-generics/app.js
+++ 8-generics/app.js
@@ -6,3 +6,10 @@ function echo(data) {
 console.log(echo("Max").length);
 console.log(echo(27).length);
 console.log(echo({ name: "Max", age: 27 }));
+// Better Generic
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho("Max").length);
+console.log(betterEcho(27));
+console.log(betterEcho({ name: "Max", age: 27 }).name);
--- 8-generics/app.ts
+++ 8-generics/app.ts
@@ -7,3 +7,11 @@ console.log(echo("Max").length)
 console.log(echo(27).length)
 console.log(echo({name: "Max", age: 27}))
 
+// Better Generic
+function betterEcho<T>(data: T) {
+	return data;
+}
+
+console.log(betterEcho("Max").length)
+console.log(betterEcho<number>(27))
+console.log(betterEcho({name: "Max", age: 27}).name)
